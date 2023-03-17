12dfbaba96f05bd2aa8e307b668ff03030660d54
ZINEBMED <zineb.ahmidou@um6p.ma>
2023-03-16 17:29:40 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - the main function 
+ *
+ * return: - 0 when successed 
+ */
+
+int main(void)
+{
+printf("Size of a char: %lu  byts(s)" , sizeof(char));
+printf("Size of a int: %lu  byts(s)" , sizeof(int));
+printf("Size of a long int: %lu  byts(s)" , sizeof(long int));
+printf("Size of a long long int: %lu  byts(s)" , sizeof(long long int));
+printf("Size of a float: %lu  byts(s)" , sizeof(float));
+
+
+}
