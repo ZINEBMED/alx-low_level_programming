7143997f062006af1d4337881f4196d9a4a8a82d
ZINEBMED <zineb.ahmidou@um6p.ma>
2023-03-18 14:00:15 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,15 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-	putchar(ch);
+		putchar(ch);
 	}
-        putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
-
