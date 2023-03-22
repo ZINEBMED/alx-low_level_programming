2a9f003e345f8c1b93fb2591ea4573e03bf21717
ZINEBMED <zineb.ahmidou@um6p.ma>
2023-03-22 10:36:54 -0700

    positive or negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -7,12 +7,19 @@
  *
  * Return: Always 0
  */
+
 void positive_or_negative(int i)
 {
 	if (i < 0)
+	{
 	printf("%d is negative\n", i);
+	}
 	else
+	{
 	printf("%d is positive\n", i);
+	}
 	if (i == 0)
+	{
 	printf("%d is zero\n", i);
+	}
 }
