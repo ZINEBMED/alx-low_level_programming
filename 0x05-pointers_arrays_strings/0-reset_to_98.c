ce294b6fe0b1da7c66560b19aff3d6c99bf338a6
ZINEBMED <zineb.ahmidou@um6p.ma>
2023-03-27 09:32:23 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - the main function
+ * reset_to_98 - that takes a pointer to an int as parameter and updates the value it points to to 98
  *
  * Return: Always 0.
  */
