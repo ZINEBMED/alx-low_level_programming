68b7aafdb587764155467e01d16341133b3f4d34
ZINEBMED <zineb.ahmidou@um6p.ma>
2023-04-05 15:11:53 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,19 @@
-include "main.h"
+#include "main.h"
 
 /**
  * _puts_recursion - function that prints a string
- * @s: string to be printed
+ * @s: string
  *
- * return: string
+ * Return: string
  */
-void _puts_recursion(char *s)
-	{
 
+void _puts_recursion(char *s)
+{
 	if (*s)
 	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
-
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-
 	else
 	_putchar('\n');
-	}
+}
