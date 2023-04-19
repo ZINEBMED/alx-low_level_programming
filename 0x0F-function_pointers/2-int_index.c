0c99406c913068e1cbf0547604477ed20df03067
ZINEBMED <zineb.ahmidou@um6p.ma>
2023-04-19 09:41:17 -0700

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,12 +12,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index;
 
-	if (size <= 0 || array == NULL || cmp == NULL)
-		return (-1);
-		for (index = 0; index < size; index++)
+	if (array && cmp)
 	{
-if (cmp(array[index]))
+	for (index = 0; index < size; index++)
+	{
+	if (cmp(array[index]) != 0)
 	return (index);
-	}
+}
+}
 	return (-1);
 }
