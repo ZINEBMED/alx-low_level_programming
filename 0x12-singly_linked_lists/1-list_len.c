4f4dac9eaf923eb5490b7a27e0b48c6a8ba6691b
ZINEBMED <zineb.ahmidou@um6p.ma>
2023-04-27 13:38:02 -0700

    task2

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -6,11 +6,13 @@
  * Return: number of elements
  */
 size_t list_len(const list_t *h)
+{
 	int count = 0;
+
 	while (h != NULL)
 {
 	count++;
 	h = h->next;
 }
-return (count);
+	return (count);
 }
