12cf7a03df862af21f86a1d1cff926c2bbeb5131
ZINEBMED <zineb.ahmidou@um6p.ma>
2023-04-27 13:33:02 -0700

    task1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include <list.h>
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: the head
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+	int count = 0;
+	while (h != NULL)
+{
+	count++;
+	h = h->next;
+}
+return (count);
+}
